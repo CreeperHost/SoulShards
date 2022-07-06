def27bfe201e94c139e477b0d5bd09d3a35ffd0c
Gigabit101 <thegigabit101@gmail.com>
2022-07-06 16:07:24 +0100

    Add jei support for creating soul shards

--- src/main/java/net/creeperhost/soulshardsrespawn/SoulShards.java
+++ src/main/java/net/creeperhost/soulshardsrespawn/SoulShards.java
@@ -17,8 +17,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 
@@ -27,7 +25,6 @@ public class SoulShards
 {
     public static final String MODID = "soulshards";
     public static final String NAME = "Soul Shards";
-    public static final Logger LOGGER = LogManager.getLogger(NAME);
     public static final ConfigSoulShards CONFIG = JsonUtil.fromJson(TypeToken.get(ConfigSoulShards.class), new File(FMLPaths.CONFIGDIR.get().toFile(), MODID + "/" + MODID + ".json"), new ConfigSoulShards());
     public static final CreativeModeTab TAB_SS = new CreativeModeTab(MODID)
     {
