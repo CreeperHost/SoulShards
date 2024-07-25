0389d71f54513e463b3a59f15979eae0937c23e9
Gigabit101 <thegigabit101@gmail.com>
2024-07-25 11:54:02 +0100

    Add durability tag to vile sword

--- src/main/java/net/creeperhost/soulshardsrespawn/datagen/GeneratorItemTags.java
+++ src/main/java/net/creeperhost/soulshardsrespawn/datagen/GeneratorItemTags.java
@@ -7,22 +7,24 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
 
 public class GeneratorItemTags extends ItemTagsProvider
 {
-    public GeneratorItemTags(PackOutput p_275343_, CompletableFuture<HolderLookup.Provider> p_275729_, CompletableFuture<TagLookup<Block>> p_275322_)
+    public GeneratorItemTags(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> providerCompletableFuture, CompletableFuture<TagLookup<Block>> tagLookupCompletableFuture)
     {
-        super(p_275343_, p_275729_, p_275322_);
+        super(packOutput, providerCompletableFuture, tagLookupCompletableFuture);
     }
 
     @Override
-    protected void addTags(HolderLookup.Provider provider)
+    protected void addTags(HolderLookup.@NotNull Provider provider)
     {
         tag(ItemTags.SWORD_ENCHANTABLE).add(RegistrarSoulShards.VILE_SWORD.get());
         tag(ItemTags.SHARP_WEAPON_ENCHANTABLE).add(RegistrarSoulShards.VILE_SWORD.get());
         tag(ItemTags.FIRE_ASPECT_ENCHANTABLE).add(RegistrarSoulShards.VILE_SWORD.get());
+        tag(ItemTags.DURABILITY_ENCHANTABLE).add(RegistrarSoulShards.VILE_SWORD.get());
         tag(SoulShardsAPI.SOUL_STEALER_ENCHANTABLE).add(RegistrarSoulShards.VILE_SWORD.get());
     }
 }
