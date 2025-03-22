efb22820a6ae44329b0c06da74f9793004103788
brandon3055 <brandon4261@gmail.com>
2025-03-22 16:24:25 +1100

    Re implemented multiblock config

--- src/main/java/net/creeperhost/soulshardsrespawn/SoulShards.java
+++ src/main/java/net/creeperhost/soulshardsrespawn/SoulShards.java
@@ -4,31 +4,26 @@ import com.google.gson.reflect.TypeToken;
 import net.creeperhost.soulshardsrespawn.client.ClientInit;
 import net.creeperhost.soulshardsrespawn.core.ConfigSoulShards;
 import net.creeperhost.soulshardsrespawn.core.RegistrarSoulShards;
-import net.creeperhost.soulshardsrespawn.core.data.Binding;
 import net.creeperhost.soulshardsrespawn.core.data.Tier;
 import net.creeperhost.soulshardsrespawn.core.util.JsonUtil;
-import net.creeperhost.soulshardsrespawn.item.ItemSoulShard;
-import net.minecraft.client.renderer.item.ItemProperties;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 
 @Mod(SoulShards.MODID)
 public class SoulShards
 {
+    public static Logger LOGGER = LogManager.getLogger();
     public static final String MODID = "soulshards";
-    public static final String NAME = "Soul Shards";
-    public static final ConfigSoulShards CONFIG = JsonUtil.fromJson(TypeToken.get(ConfigSoulShards.class), new File(FMLPaths.CONFIGDIR.get().toFile(), MODID + "/" + MODID + ".json"), new ConfigSoulShards());
+    public static final File CONFIG_DIR = new File(FMLPaths.CONFIGDIR.get().toFile(), MODID);
+    public static final ConfigSoulShards CONFIG = JsonUtil.fromJson(TypeToken.get(ConfigSoulShards.class), new File(CONFIG_DIR, MODID + ".json"), new ConfigSoulShards());
 
     public SoulShards()
     {
@@ -42,5 +37,6 @@ public class SoulShards
         RegistrarSoulShards.CREATIVE_TAB.register(eventBus);
 
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> ClientInit::init);
+        ConfigSoulShards.handleMultiblock();//Ensure multiblock config is loaded on startup
     }
 }
