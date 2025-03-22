145cb8942199ed6b12113369b716c5d6b180a627
brandon3055 <brandon4261@gmail.com>
2025-03-22 15:41:29 +1100

    Re implemented multiblock config

--- src/main/java/net/creeperhost/soulshardsrespawn/SoulShards.java
+++ src/main/java/net/creeperhost/soulshardsrespawn/SoulShards.java
@@ -4,27 +4,25 @@ import com.google.gson.reflect.TypeToken;
 import net.creeperhost.soulshardsrespawn.client.ClientInit;
 import net.creeperhost.soulshardsrespawn.core.ConfigSoulShards;
 import net.creeperhost.soulshardsrespawn.core.RegistrarSoulShards;
-import net.creeperhost.soulshardsrespawn.core.data.Binding;
 import net.creeperhost.soulshardsrespawn.core.data.Tier;
 import net.creeperhost.soulshardsrespawn.core.util.JsonUtil;
-import net.creeperhost.soulshardsrespawn.item.ItemSoulShard;
-import net.minecraft.client.renderer.item.ItemProperties;
-import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.DistExecutor;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.loading.FMLPaths;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 
 @Mod(SoulShards.MODID)
 public class SoulShards
 {
+    public static Logger LOGGER = LogManager.getLogger();
     public static final String MODID = "soulshards";
-    public static final ConfigSoulShards CONFIG = JsonUtil.fromJson(TypeToken.get(ConfigSoulShards.class), new File(FMLPaths.CONFIGDIR.get().toFile(), MODID + "/" + MODID + ".json"), new ConfigSoulShards());
+    public static final File CONFIG_DIR = new File(FMLPaths.CONFIGDIR.get().toFile(), MODID);
+    public static final ConfigSoulShards CONFIG = JsonUtil.fromJson(TypeToken.get(ConfigSoulShards.class), new File(CONFIG_DIR, MODID + ".json"), new ConfigSoulShards());
 
     public SoulShards(IEventBus eventBus)
     {
@@ -36,5 +34,6 @@ public class SoulShards
         RegistrarSoulShards.CREATIVE_TAB.register(eventBus);
 
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> ClientInit::init);
+        ConfigSoulShards.handleMultiblock();//Ensure multiblock config is loaded on startup
     }
 }
